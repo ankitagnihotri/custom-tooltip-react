af09c1411c5ccb656b04e0dbe9ef52d36eba239f
Ankit Agnihotri <ankit@Ankits-MacBook-Pro.local>
2019-04-02 18:47:46 +0530

    intermediate changes

--- src/lib/Tooltip.js
+++ src/lib/Tooltip.js
@@ -3,47 +3,26 @@
  * @Date:   2019-04-01T03:07:49+05:30
  * @Email:  ankit@minance.com
  * @Last modified by:   ankit
- * @Last modified time: 2019-04-02T18:16:17+05:30
+ * @Last modified time: 2019-04-02T18:47:22+05:30
  * @Copyright: Minance Technologies Private Limited
  */
 
- import React from 'react';
- import './Tooltip.css';
+import React from 'react';
+import './Tooltip.css';
 
- class Tooltip extends React.Component{
-   constructor(props) {
-     super(props)
-
-     this.state = {
-       displayTooltip: false
-     }
-     this.hideTooltip = this.hideTooltip.bind(this)
-     this.showTooltip = this.showTooltip.bind(this)
-   }
-
-   hideTooltip() {
-     this.setState({displayTooltip: false})
-
-   }
-   showTooltip() {
-     this.setState({displayTooltip: true})
-   }
-
-   render() {
-     let position = this.props.position
-     return (
-       <div className='CustomTooltip' onMouseLeave={this.hideTooltip}>
-      {this.state.displayTooltip &&
+class Tooltip extends React.Component{
+  
+  render() {
+    let position = this.props.position
+    return (
+      <div className='CustomTooltip'>
         <div className={`CustomTooltipBubble CustomTooltip-${position}`}>
-         <div className='CustomTooltipMessage' style={this.props.style}>{this.props.hoverText}</div>
-         </div>
-      }
-      <div onMouseOver={this.showTooltip}>
-        {this.props.children}
+        <div className='CustomTooltipMessage' style={this.props.style}>{this.props.hoverText}</div>
+        </div>
+        <div>{this.props.children}</div>
       </div>
-    </div>
-     );
-   }
- };
+    );
+  }
+};
 
- export default Tooltip;
+export default Tooltip;

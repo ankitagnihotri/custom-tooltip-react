a4360fd41115cf8d3d8abd93eb90cb5345f366ac
Ankit Agnihotri <ankit@Ankits-MacBook-Pro.local>
2019-04-05 04:22:14 +0530

    --version 0.1.2.

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import './Tooltip.css';
 
-class Tooltip extends Component{
-  
+class Tooltip extends Component{  
   render() {
     return (
       <div className='CustomTooltip'>
-        <div className={`CustomTooltipBubble CustomTooltip-${this.props.placement}`} 
-             style={{color: `${this.props.tipColor}`}}>
-          <div className='CustomTooltipTextPaper CustomTooltipText' 
-                style={this.props.tooltipTextPaperStyle}>{this.props.tooltipMessage}
+        <div className={`CustomTooltipBubble CustomTooltip-${this.props.placement}`} style={{color: `${this.props.tipColor}`}}>
+          <div className='CustomTooltipTextPaper' style={this.props.tooltipTextPaperStyle}>
+            <span className='CustomTooltipText'>{this.props.tooltipMessage}</span>
           </div>
         </div>
         <span>
